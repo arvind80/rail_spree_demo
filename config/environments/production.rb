e74be99e2c73f73f39dae2e2af23b45d3fc29c0e
deployer <deployer@108-171-177-115.static.cloud-ips.com>
2012-04-01 12:35:24 +0000

    final commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,7 @@ RailsdogRadio::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
 
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
