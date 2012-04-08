ebc087e79bfac28bf3fd78a90794663c432124e6
deployer <deployer@108-171-177-115.static.cloud-ips.com>
2012-04-08 08:33:02 +0000

    init

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,31 +1,67 @@
-worker_processes  2
-rails_env = ENV['RAILS_ENV'] || 'production'
+# Sample verbose configuration file for Unicorn (not Rack)
+#
+# This configuration file documents many features of Unicorn
+# that may not be needed for some applications. See
+# http://unicorn.bogomips.org/examples/unicorn.conf.minimal.rb
+# for a much simpler configuration file.
+#
+# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
+# documentation.
 
-working_directory "/home/deployer/demo/current" 
+# Use at least one worker per core if you're on a dedicated server,
+# more will usually help for _short_ waits on databases/caches.
+worker_processes 4
 
-listen "/home/deployer/demo/shared/unicorn.sock", :backlog => 64
+# Since Unicorn is never exposed to outside clients, it does not need to
+# run on the standard HTTP port (80), there is no reason to start Unicorn
+# as root unless it's from system init scripts.
+# If running the master process as root and the workers as an unprivileged
+# user, do this to switch euid/egid in the workers (also chowns logs):
+# user "unprivileged_user", "unprivileged_group"
+
+# Help ensure your application will always spawn in the symlinked
+# "current" directory that Capistrano sets up.
+working_directory "/home/deployer/demo/current" # available in 0.94.0+
 
 
+# listen on both a Unix domain socket and a TCP port,
+# we use a shorter backlog for quicker failover when busy
+listen "/tmp/.sock", :backlog => 64
+listen 8080, :tcp_nopush => true
+
+# nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
 
-user 'deployer', 'staff'
+# feel free to point this anywhere accessible on the filesystem
+pid "/home/deployer/demo/shared/pids/unicorn.pid"
 
+# By default, the Unicorn logger will write to stderr.
+# Additionally, ome applications/frameworks log to stderr or stdout,
+# so prevent them from going to /dev/null when daemonized here:
+stderr_path "/home/deployer/demo/shared/log/unicorn.stderr.log"
+stdout_path "/home/deployer/demo/shared/log/unicorn.stdout.log"
 
-shared_path = "/home/deployer/demo/shared"
-pid "#{shared_path}/pids/unicorn.pid"
-stderr_path "#{shared_path}/log/unicorn.stderr.log"
-
-stdout_path "#{shared_path}/log/unicorn.stdout.log"
-
-
+# combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
+# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) and
   GC.copy_on_write_friendly = true
+
 before_fork do |server, worker|
   # the following is highly recomended for Rails + "preload_app true"
   # as there's no need for the master process to hold a connection
   defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect! 
+    ActiveRecord::Base.connection.disconnect!
+
+  # The following is only recommended for memory/DB-constrained
+  # installations.  It is not needed if your system can house
+  # twice as many worker_processes as you have configured.
+  #
+  # # This allows a new master process to incrementally
+  # # phase out the old master process with SIGTTOU to avoid a
+  # # thundering herd (especially in the "preload_app false" case)
+  # # when doing a transparent upgrade.  The last worker spawned
+  # # will then kill off the old master process with a SIGQUIT.
   # old_pid = "#{server.config[:pid]}.oldbin"
   # if old_pid != server.pid
   #   begin
@@ -34,15 +70,23 @@ before_fork do |server, worker|
   #   rescue Errno::ENOENT, Errno::ESRCH
   #   end
   # end
+  #
+  # Throttle the master from forking too quickly by sleeping.  Due
+  # to the implementation of standard Unix signal handlers, this
+  # helps (but does not completely) prevent identical, repeated signals
+  # from being lost when the receiving process is busy.
   # sleep 1
 end
+
 after_fork do |server, worker|
   # per-process listener ports for debugging/admin/migrations
   # addr = "127.0.0.1:#{9293 + worker.nr}"
   # server.listen(addr, :tries => -1, :delay => 5, :tcp_nopush => true)
+
   # the following is *required* for Rails + "preload_app true",
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
+
   # if preload_app is true, then you may also want to check and
   # restart any other shared sockets/descriptors such as Memcached,
   # and Redis.  TokyoCabinet file handles are safe to reuse
